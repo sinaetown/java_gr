ffb722bf41eb9358dc2892fe37f47faf74cc1eb6
sinaetown <ksw08215@gmail.com>
2023-12-27 12:05:58 +0900

    1227 json files created

--- src/C17ExceptionFileParsing/C1703JsonWithJacksonRaw.java
+++ src/C17ExceptionFileParsing/C1703JsonWithJacksonRaw.java
@@ -1,18 +1,24 @@
 package C17ExceptionFileParsing;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.IOException;
-import java.util.HashMap;
+import java.nio.file.Paths;
+import java.util.*;
 
 public class C1703JsonWithJacksonRaw {
     public static void main(String[] args) {
-        ObjectMapper mapper = new OBjectMapper();
-        try{
+        ObjectMapper mapper = new ObjectMapper();
+        try {
 //            readTree 통해서 json을 계층적 트리 구조 형태로 변환
-            JsonNode data1- mapper.readTree(Paths.get());
+            JsonNode data1 = mapper.readTree(Paths.get("src/C17ExceptionFileParsing/testdata").toFile());
             Map<String, String> studentMap = new HashMap<>();
-            studentMap.,put()
-        }
-        catch(IOException e){
+            studentMap.put("name", data1.get("name").asText());
+            studentMap.put("classNumber", data1.get("classNumber").asText());
+            studentMap.put("city", data1.get("city").asText());
+            System.out.println(studentMap);
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
---
+++ src/C17ExceptionFileParsing/C1705JsonWithJacksonHttp.java
@@ -0,0 +1,7 @@
+package C17ExceptionFileParsing;
+
+public class C1705JsonWithJacksonHttp {
+    public static void main(String[] args) {
+
+    }
+}
